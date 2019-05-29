43d2d68e484760f61ba2bbb919acbddf8992ca81
toledompm <toledompm@gmail.com>
2019-05-29 19:18:42 -0300

    Finish guarda_posicoes bug fix

--- batalha_naval.py
+++ batalha_naval.py
@@ -8,12 +8,12 @@ def leia_arquivo(nome):
             posicoes[i] = i.split(';')[1].split('|')
         elif i[0] == 'T':
             torpedos = i.split(';')[1].split('|')
+    arquivo.close()
     return posicoes,torpedos
 
 def guarda_posicoes(posicoes):
-    import string
+    overwrite = False
     letras = "ABCDEFGHIJLMNOPQRSTUVXZ"
-    sequencia = "ABCDEFGHIJLMNOP"
     tamanhos = {'1':1,'2':1,'3':1,'4':1}
     idNavio = 0
     posicoes_ocupadas = {}
@@ -24,18 +24,22 @@ def guarda_posicoes(posicoes):
             if pos[-1] in 'HV':
                 if pos[-1] == 'H':
                     vertical = False
-                pos = pos[:len(pos) - 1]
+                pos = pos[:-1]
+            h,v = [0,0]
             for i in range(tam):
-                h,v = [0,0]
                 if vertical:
                     v += i 
                 else:
                     h += i
-                novaLinha = letras[letras.find(pos[0])+v]
-                novaColuna = str(int(pos[1:])+h)
-                novaPosicao =  novaLinha+novaColuna 
+                novaLinha = letras.find(pos[0])+v
+                novaColuna = int(pos[1:])+h
+                if novaLinha > 15 or novaLinha < 0 or novaColuna > 15 or novaColuna < 0:
+                    print('position nonexistant')
+                novaPosicao =  novaLinha+';'+novaColuna
                 if novaPosicao in posicoes_ocupadas:
-                    print('overwrite pieces validation')
+                    overwrite = True
                 posicoes_ocupadas[novaPosicao] = idNavio
             idNavio += 1
-    return
+    if overwrite:
+        print('overwrite error')
+    return posicoes_ocupadas
